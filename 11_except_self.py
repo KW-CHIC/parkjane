1208eb7c27615bdd073a0a6bfba667a479b658bd
Jane Park <77182650+jane-datascience@users.noreply.github.com>
2022-08-15 00:09:58 +0900

    Update 11_except_self.py

--- 11_except_self.py
+++ 11_except_self.py
@@ -1,3 +1,5 @@
+#배열을 입력받아 output[i]가 자신을 제외한 나머지 모든 요소의 곱셈 결과가 되도록 출력하라.
+
 def except_self(nums):
     result = []
     j = 1
@@ -9,6 +11,6 @@ def except_self(nums):
     for i in range(len(nums)-1, 0-1, -1):
         result[i] = result[i] * j
         j=j*nums[i]
-return result
+    return result
 
-print(except_self([7,1,5,3,6,4])
+print(except_self([1,2,3,4]))
