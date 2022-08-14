a440e1130c5cf299a30c4662383ee0527a163bb8
Jane Park <77182650+jane-datascience@users.noreply.github.com>
2022-08-15 00:04:03 +0900

    Update 10_pair_sum.py

--- 10_pair_sum.py
+++ 10_pair_sum.py
@@ -1,10 +1,13 @@
+#n개의 페어를 이용한 min(a,b)의 합으로 만들 수 있는 가장 큰 수를 출력하라.
+
+
 def pair_sum(nums):
   sum=0
-  nums.sort(0)
+  nums.sort()
   
   for i, j in enumerate(nums):
     if i%2 == 0:
       sum+=j
   return sum
 
-print(pair_sum([1,4,3,2])
+print(pair_sum([1,4,3,2]))
