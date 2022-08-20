23787aca97a53f8d2cee36c787e1802f57699f22
Jane Park <77182650+jane-datascience@users.noreply.github.com>
2022-08-20 13:00:08 +0900

    Create 14_merge_two_sorted_lists.py

---
+++ 14_merge_two_sorted_lists.py
@@ -0,0 +1,8 @@
+#213p
+
+def merge_two_lists(self, l1 : ListNode, l2 : ListNode) -> ListNode:
+  if(not l1) or (l2 and l1.val > l2.val):
+    l1,l2 - l2,l1
+  if l1:
+    l1.next = self.merge_two_lists(l1.next, l2)
+  return l1
